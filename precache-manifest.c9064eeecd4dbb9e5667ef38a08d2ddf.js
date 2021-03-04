e2098b6fdcf3742c54c0389f139451f867fb518a
martinchary <martochary@gmail.com>
2021-03-04 18:46:37 -0300

    Updates

---
+++ precache-manifest.c9064eeecd4dbb9e5667ef38a08d2ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea4e433e0729d623427ca78f26372bfc",
+    "url": "/backoffice/index.html"
+  },
+  {
+    "revision": "6c261235529f41bb5811",
+    "url": "/backoffice/static/css/main.7ed35391.chunk.css"
+  },
+  {
+    "revision": "5d19765849411d829b15",
+    "url": "/backoffice/static/js/2.ea176255.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/backoffice/static/js/2.ea176255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c261235529f41bb5811",
+    "url": "/backoffice/static/js/main.4caa3edc.chunk.js"
+  },
+  {
+    "revision": "a41262c358923216a228",
+    "url": "/backoffice/static/js/runtime-main.ad4ec29f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/backoffice/static/media/logo.5d5d9eef.svg"
+  }
+]);
